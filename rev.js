dd9fe45b8498883dfe88bee3241651ca85325f22
Shaddy <shadybett540@gmail.com>
2023-10-18 23:44:21 +0300

    additions

--- rev.js
+++ rev.js
@@ -3,3 +3,7 @@ document.getElementById('nema')
 .addEventListener('click',function (){
     console.log('nemaste',++count);
 });
+document.getElementById('btn4').addEventListener('click',function(){
+    console.log('Thank you')
+
+})
